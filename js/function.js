8dfe42f5ee70f2ecef3bd0b54d85e6e02c59f3b2
Alumno_invitado <diegomcano18@gmail.com>
2023-01-30 21:16:09 -0300

    identado

--- js/function.js
+++ js/function.js
@@ -1,12 +1,12 @@
 
-	function bigImg(x) {
-			  x.style.height = "200px";
-			  x.style.width = "160px";
-			}
+function bigImg(x) {
+	x.style.height = "200px";
+	x.style.width = "160px";
+}
 
-	function normalImg(x) {
-			  x.style.height = "180px";
-			  x.style.width = "140px";
-			}	
+function normalImg(x) {
+	x.style.height = "180px";
+	x.style.width = "140px";
+}	
 
 
